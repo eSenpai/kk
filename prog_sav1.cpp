acfe627b9bbb386513c300f497b6f62d91c8125d
eSenpai <eSenpaii@gmail.com>
2015-11-16 08:56:42 +0200

    Update prog_sav1.cpp

--- prog_sav1.cpp
+++ prog_sav1.cpp
@@ -1,5 +1,4 @@
 //Dominykas Litvaitis INF 5
-//Yra keistenybe, kadangi is gautu neigiamu skaiciu yra traukiama saknis, kai x = -1 ir x = 1
 
 #include <iostream>  //Kad galetume naudoti pagrindines c++ kalbos dalis
 #include <cmath>     //Kad galetume naudoti pow
@@ -10,14 +9,25 @@ int main(){
 	double y;        //Musu ieskoma reiksme
 	double t;        
 	double x = -1;   //Kintamasis, kuris kinta h zingsniu
-	int h = 1;       
-
+	int h = 1;
+	int komanda = 1; //Kintamasis, su kuriuo bus isjungiama programa
+	cout << "Iveskite x: ";
+	cin >> x;
+	cout << "Iveskite h: ";
+	cin >> h;
+	cout << "|--------------------------------------------|" << endl;
 	do{
 		t = (sqrt(pow(sin(x), 2))) / (x - 2);   //Surandame t reiksme
-		y = sqrt(2 * t + x);                    //Naudojant gauta t reiksme, surandame y
-		cout << "y = " << y << endl;            //Isspausdiname y
+		if (t < 0){
+			cout << "|t = neigiamas | y = sprendinio nera | x = " << x << " |" << endl;
+		} else {
+			y = sqrt(2 * t + x);                    //Naudojant gauta t reiksme, surandame y
+			cout << "|t = " << t << "        | y = " << y << "               | x = " << x << " |" << endl;            //Isspausdiname y
+		}
 		x += h;                                 //Prie x pridedame h (h = 1)
 	} while (x <= 1);                           //Darome cikla iki kada x bus lygus 2
-	
-	while (1);       //Naudojame cikla, kad programa iskarto neuzsidarytu
+	cout << "|--------------------------------------------|" << endl;
+	while (komanda != 0){       //Naudojame cikla, kad programa uzsidarytu, kai irasomas 0
+		cin >> komanda;
+	}
 }
